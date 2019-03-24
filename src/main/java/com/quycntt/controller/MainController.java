f17123c9ee3a6f565b21fb8c1f52ee58c8fcc5b2
dangvanquy20041998 <47421943+dangvanquy20041998@users.noreply.github.com>
2019-03-24 23:31:03 +0700

    Add files via upload

--- src/main/java/com/quycntt/controller/MainController.java
+++ src/main/java/com/quycntt/controller/MainController.java
@@ -1,11 +1,16 @@
 package com.quycntt.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.quycntt.demo;
 import com.quycntt.entity.Location;
 import com.quycntt.serviceimp.LocationServiceImp;
 
@@ -14,6 +19,9 @@ public class MainController {
 	
 	@Autowired
 	private LocationServiceImp locationServiceImp;
+	
+	@Autowired
+	private demo demoService;
 	
 	@GetMapping("/")
 	public String index() {
@@ -29,5 +37,19 @@ public class MainController {
 		location.setPosition(location1);
 		
 		locationServiceImp.saveLocation(location);
+	}
+	
+	@GetMapping("/position/{lat}/{lng}")
+	@ResponseBody
+	public Location getPosition(@PathVariable(value="lat") String lat, @PathVariable(value="lng") String lng) {
+		try {
+			Location location = demoService.getLocationInfo(lat, lng);
+			locationServiceImp.saveLocation(location);
+			return demoService.getLocationInfo(lat, lng);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
